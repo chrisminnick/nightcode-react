fa8f4a81f1f787faf349423566fc59abfdba8fae
Chris Minnick <chris@minnick.com>
2016-06-08 15:38:30 -0700

    adding to gulp scripts

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+const gulp          = require('gulp');
+const del           = require('del');
+const webpack       = require('webpack-stream');
+const webserver     = require('gulp-webserver');
+const DIST          = 'dist';
+
+//clean
+gulp.task('clean', function() {
+    console.log ('removing dist directory');
+    return del([
+        DIST
+    ]);
+});
+
+//copy html files to dist
+gulp.task('copy', function() {
+    return gulp.src('src/*.html').pipe(gulp.dest(DIST));
+});
+
+//run
+gulp.task('run', function() {
+    gulp.src('src')
+        .pipe(webserver({
+            livereload: true,
+            open: true
+        }));
+});
+
+//build the dist directory
+gulp.task('webpack', function() {
+    return gulp.src('src/scripts/app.js')
+        .pipe(webpack(require('./webpack.config.js')))
+        .pipe(gulp.dest('dist/scripts/'));
+});
+
+//build
+gulp.task('build', gulp.series('clean',gulp.parallel('webpack','copy'),'run'), function(done) {
+    console.log('BUILD COMPLETE');
+    done();
+
+});
+// default task
+gulp.task('default', gulp.series('build'), function(done) {
+    console.log('BUILD OK');
+    done();
+});
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    entry  : './src/scripts/app.js',
+    output : {
+        filename : 'app.js'
+    },
+    module : {
+        loaders: [ {
+            test   : /.js$/,
+            loader : 'babel-loader',
+            query: {
+                presets: ['es2015','react']
+            }
+        }
+        ]
+    }/*,
+     plugins: [
+     new webpack.DefinePlugin({
+     'process.env': {
+     'NODE_ENV': '"production"'
+     }
+     })
+     ]*/
+
+};
