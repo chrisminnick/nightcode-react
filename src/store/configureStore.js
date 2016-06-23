8520f04e70398b4c04fec453a6229d9f155013c6
Chris Minnick <chris@minnick.com>
2016-06-22 17:36:45 -0700

    updated configurestore with redux-ummutable-state-invariant and redux dev tools

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,9 +1,11 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import swimCalc from '../reducers';
+import invariant from 'redux-immutable-state-invariant';
 
 export default function configureStore(initialState){
-    const store = createStore(swimCalc, initialState,
-        window.devToolsExtension && window.devToolsExtension()
-    );
+    const store = createStore(swimCalc, initialState, compose(
+        applyMiddleware(invariant()),
+        window.devToolsExtension ? window.devToolsExtension() : f => f
+    ));
     return store;
 }
